77de4198ccd6f9768134c3e5b0ef3e2971358aa7
neil <orderllrobbieesquire@gmail.com>
2022-07-16 10:18:53 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# neil stuff  
+
+npx create-react-app tlv3  
+
+cd tlv3  
+
+npm start  
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Data Asset Exchange
+        </h3>
+        <p>Currently publishing daily clusters of DYDX asset pairs.  Tokens available via Zetonium.</p>
       </header>
     </div>
   );
