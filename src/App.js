a9d90bf6a7eb71ee2dfbafe64774cdd292038782
neil <orderllrobbieesquire@gmail.com>
2022-07-18 16:22:01 -0400

    clean

--- src/App.js
+++ src/App.js
@@ -115,23 +115,26 @@ function App() {
 
               <>
 
-                <section class="section">
-                  <nav class="breadcrumb is-centered is-large has-arrow-separator" aria-label="breadcrumbs">
-                    <ul>
-                      {isViewingOverview == true ? (
-                        <li class="is-active"><a href="#">Overview</a></li>
-                      ) : (
-                        <li><a href="#" onClick={e => onClickedHome(e)}>Overview</a></li>
-                      )}
-                      {isViewingRepos== true ? (
-                        <li class="is-active"><a href="#">Install Repos</a></li>
-                      ) : (
-                        <li><a href="#" onClick={e => onClickedRepos(e)}>Install Repos</a></li>
-                      )}
+                <section class="hero is-info mb-3">
+                  <div class="hero-body">
+                    <nav class="breadcrumb is-centered is-large has-arrow-separator" aria-label="breadcrumbs">
+                      <ul>
+                        {isViewingOverview == true ? (
+                          <li class="is-active has-text-warning-dark"><a href="#">Overview</a></li>
+                        ) : (
+                          <li><a href="#" onClick={e => onClickedHome(e)}>Overview</a></li>
+                        )}
+                        {isViewingRepos== true ? (
+                          <li class="is-active has-text-warning-dark"><a href="#">Install Repos</a></li>
+                        ) : (
+                          <li><a href="#" onClick={e => onClickedRepos(e)}>Install Repos</a></li>
+                        )}
 
-                      <li><a href="#">PDS Portfolio</a></li>
-                    </ul>
-                  </nav>            
+                        <li><a href="#">PDS Portfolio</a></li>
+                      </ul>
+                    </nav>                                
+                    <p class="title">It's really as easy as install, run, play, mint.</p>
+                  </div>
                 </section>
 
 
