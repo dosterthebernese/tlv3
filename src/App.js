f2a870fc896ffb4f74fd9f8a4daac7cfc2a08ea5
neil <orderllrobbieesquire@gmail.com>
2022-07-18 11:49:35 -0400

    upd

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ function App() {
 
   const [isViewingDocumentation, setIsViewingDocumentation] = useState(false);
   const [isViewingHome, setIsViewingHome] = useState(true);
+  const [isViewingOverview, setIsViewingOverview] = useState(true);
+  const [isViewingRepos, setIsViewingRepos] = useState(true);
 
   async function onClickedDocumentation(event) {
     setIsViewingHome(false)
+    setIsViewingOverview(false)
+    setIsViewingRepos(false)
     setIsViewingDocumentation(true);
 
   };
 
   async function onClickedHome(event) {
     setIsViewingDocumentation(false);
+    setIsViewingRepos(false);
+    setIsViewingHome(true)
+    setIsViewingOverview(true)
+  };
+
+  async function onClickedRepos(event) {
+    setIsViewingDocumentation(false);
+    setIsViewingOverview(false)
+    setIsViewingRepos(true);
     setIsViewingHome(true)
   };
 
@@ -88,105 +101,227 @@ function App() {
       <section class="section">
         <div class="container">
 
-          <div class="columns">
 
             {isViewingDocumentation == true && (
-              <div class="column">              
-                <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">Haha!  Yes, we skip the silliness and send you to Discord.  Why would you want anything else?</p>
-                <iframe src="https://discord.com/widget?id=928022156888125520&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-              </div>              
-            )}
+              <div class="columns">
+                <div class="column">              
+                  <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">Haha!  Yes, we skip the silliness and send you to Discord.  Why would you want anything else?</p>
+                  <iframe src="https://discord.com/widget?id=928022156888125520&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>              
+              </div>
+              )}
 
             {isViewingHome == true && (
+
               <>
 
-                <div class="column">              
-                  <article class="message is-dark">
-                    <div class="message-header">
-                      <p>Be a Llama</p>
-                    </div>
-                    <div class="message-body">
-                      <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        What follows are instructions for spinning up a spartan linux box (Ubuntu 20.04, due to some Elixir issues that are not ready for 22.04).                    
-                      </p>
-                      <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        Within minutes, you can have a POC for your own <b>data as a product</b>, <b>collaborative commerce</b> framework, with licensing on chain and royalty management enabled.                    
-                      </p>
-                      <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        You will easily be able to swap out our simple data ideas for your own.  If you are evaluating an on chain data campaign, this is the fastest way to get to a POC.
-                      </p>
-                    </div>
-                  </article>
+                <section class="section">
+                  <nav class="breadcrumb is-centered is-large has-arrow-separator" aria-label="breadcrumbs">
+                    <ul>
+                      {isViewingOverview == true ? (
+                        <li class="is-active"><a href="#">Overview</a></li>
+                      ) : (
+                        <li><a href="#" onClick={e => onClickedHome(e)}>Overview</a></li>
+                      )}
+                      {isViewingRepos== true ? (
+                        <li class="is-active"><a href="#">Install Repos</a></li>
+                      ) : (
+                        <li><a href="#" onClick={e => onClickedRepos(e)}>Install Repos</a></li>
+                      )}
+
+                      <li><a href="#">PDS Portfolio</a></li>
+                    </ul>
+                  </nav>            
+                </section>
+
+
+                <div class="columns">
+
+
+                
+
+
+
+                    {isViewingOverview == true && (
+
+                      <>
+
+                        <div class="column">   
+
+                          <div class="card">
+                            <header class="card-header">
+                              <p class="card-header-title">
+                                DIY in 3 Easy Steps
+                              </p>
+                              <button class="card-header-icon" aria-label="more options">
+                                <span class="icon">
+                                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                              </button>
+                            </header>
+
+                            <div class="card-content">
+                              <div class="content">
+                                <p class="mb-3">
+                                  What follows are instructions for spinning up a spartan linux box (Ubuntu 20.04, due to some Elixir issues that are not ready for 22.04).                    
+                                </p>
+                                <p class="mb-3">
+                                  Within minutes, you can have a POC for your own <b>data as a product</b>, <b>collaborative commerce</b> framework, with licensing on chain and royalty management enabled.                    
+                                </p>
+                                <p class="mb-3">
+                                  You will easily be able to swap out our simple data ideas for your own.  If you are evaluating an on chain data campaign, this is the fastest way to get to a POC.
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        
+
+                        </div>
+
+
+                        <div class="column">          
+
+
+
+                          <div class="card">
+                            <header class="card-header">
+                              <p class="card-header-title">
+                                Open Source
+                              </p>
+                              <button class="card-header-icon" aria-label="more options">
+                                <span class="icon">
+                                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                              </button>
+                            </header>
+
+                            <div class="card-content">
+                              <div class="content">
+                                 <p class="mb-3">
+                                  With a few repos and some command line installs, you can easily create digital assets, publish them to a vault and register them on chain, combine them into a Token, and offer them to your target customers.  
+                                  </p>
+                                 <p class="mb-3">
+                                  These are the same code repos we at TradeLlama use for our clustering data sets.  
+                                  </p>
+                              </div>
+                            </div>
+                          </div>
+
+
+                        </div>
+
+                      </>
+
+
+                    )}
+
+
+                    {isViewingRepos == true && (
+
+
+                      <>
+
+
+
+
+                        <div class="column">
+
+
+
+                          <div class="card">
+                            <header class="card-header">
+                              <p class="card-header-title">
+                                Your Data Magic
+                              </p>
+                              <button class="card-header-icon" aria-label="more options">
+                                <span class="icon">
+                                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                              </button>
+                            </header>
+
+                            <div class="card-content">
+                              <div class="content">
+                                <a class="button is-link" href="https://github.com/dosterthebernese/tldydx">Repo at GitHub</a>
+                                 <p class="mt-3 mb-3">
+                                  This will give you a few simple Agents (it's elixir, don't worry) that will pull data from DYDX (it's free), and store in a database.  Later, you'll make some assets with this data.
+                                </p>
+
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+
+
+
+                        <div class="column">
+
+                          <div class="card">
+                            <header class="card-header">
+                              <p class="card-header-title">
+                                Build a Data Asset
+                              </p>
+                              <button class="card-header-icon" aria-label="more options">
+                                <span class="icon">
+                                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                              </button>
+                            </header>
+
+                            <div class="card-content">
+                              <div class="content">
+                                <a class="button is-link" href="https://github.com/dosterthebernese/pdsr">Repo at GitHub</a>
+                                 <p class="mt-3 mb-3">
+                                A simple set of tools for taking data out of postgres, making some clusters, and then creating assets - in this case, SVGs, PNGs, and CSVs.
+                                </p>
+
+                              </div>
+                            </div>
+                          </div>
+
+
+                        </div>
+
+                        <div class="column">
+
+
+
+                          <div class="card">
+                            <header class="card-header">
+                              <p class="card-header-title">
+                                Minting
+                              </p>
+                              <button class="card-header-icon" aria-label="more options">
+                                <span class="icon">
+                                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                              </button>
+                            </header>
+
+                            <div class="card-content">
+                              <div class="content">
+                                <a class="button is-link" href="https://github.com/dosterthebernese/pds">Repo at GitHub</a>
+                                 <p class="mt-3 mb-3">
+                                  A command line toolset for registering your assets on chain, bundling them into tokens, and offering them to users.
+                                </p>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+                      </>
+                    )}
+
+          
+
                 </div>
-
-
-                <div class="column">          
-                  <article class="message is-dark">
-                    <div class="message-header">
-                      <p>3 Open Source Repos</p>
-                    </div>
-                    <div class="message-body">
-                       <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        With a few repos and some command line installs, you can easily create digital assets, publish them to a vault and register them on chain, combine them into a Token, and offer them to your target customers.  
-                        </p>
-                       <p class="mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        These are the same code repos we at TradeLlama use for our clustering data sets.  
-                        </p>
-                    </div>
-                  </article>
-                </div>
-
-      
-                <div class="column">
-                  <article class="message is-dark">
-                    <div class="message-header">
-                      <p>Install TLDYDX</p>
-                    </div>
-                    <div class="message-body">
-                      <a class="button is-link" href="https://github.com/dosterthebernese/tldydx">Repo at GitHub</a>
-                       <p class="mt-3 mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        This will give you a few simple Agents (it's elixir, don't worry) that will pull data from DYDX (it's free), and store in a database.  Later, you'll make some assets with this data.
-                      </p>
-                    </div>
-                  </article>
-                </div>
-
-
-
-                <div class="column">
-                  <article class="message is-dark">
-                    <div class="message-header">
-                      <p>Install PDSR</p>
-                    </div>
-                    <div class="message-body">
-                      <a class="button is-link" href="https://github.com/dosterthebernese/pdsr">Repo at GitHub</a>
-                       <p class="mt-3 mb-3 is-size-5 has-text-weight-light has-text-dark">
-                      A simple set of tools for taking data out of postgres, making some clusters, and then creating assets - in this case, SVGs, PNGs, and CSVs.
-                      </p>
-                    </div>
-                  </article>
-                </div>
-
-                <div class="column">
-                  <article class="message is-dark">
-                    <div class="message-header">
-                      <p>Install PDS</p>
-                    </div>
-                    <div class="message-body">
-
-                      <a class="button is-link" href="https://github.com/dosterthebernese/pds">Repo at GitHub</a>
-                       <p class="mt-3 mb-3 is-size-5 has-text-weight-light has-text-dark">
-                        A command line toolset for registering your assets on chain, bundling them into tokens, and offering them to users.
-                      </p>
-
-                    </div>
-                  </article>
-                </div>
-
               </>
+
             )}
 
-          </div>
         </div>
       </section>
 
